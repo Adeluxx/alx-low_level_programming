fa55d5102d5c2688a0f8cbc2b78ede546c4946b3
Adeluxx <getahunaderajew@gmail.com>
2023-03-20 04:52:02 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - entry poit
- * return: always 0 (success)
+ * main - this is the main function
+ *
+ * Return: Always 0
  */
+
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
