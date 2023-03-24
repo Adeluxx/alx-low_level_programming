349591850e95cc5b0eaf36c65a1183e6c0e0e648
Adeluxx <getahunaderajew@gmail.com>
2023-03-24 04:55:06 -0700

    print num

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,10 +15,11 @@ void print_number(int n)
 	{
 		n *= -1;
 		k = n;
-		_putchar('-');
+		
+		putchar('-');
 	}
 	k /= 10;
 	if (k != 0)
 		print_number(k);
-	_putchar((unsigned int) n % 10 + '0');
+	putchar((unsigned int) n % 10 + '0');
 }
