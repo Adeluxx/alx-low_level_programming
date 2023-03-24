0ce561b720a5b33c2c33093a630b6e74553dd3d6
Adeluxx <getahunaderajew@gmail.com>
2023-03-24 00:25:48 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,10 +13,6 @@ void print_square(int size)
 
 	if (size <= 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
 		for (x = 0; x < size; x++)
 		{
 			for (y = 0; y < size; y++)
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,4 +11,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n)
+void print_number(int n);
+
+#endif
