f51f2f9a217049475c574289c9f6332d2e88604b
Adeluxx <getahunaderajew@gmail.com>
2023-03-24 01:58:03 -0700

    most num

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,17 +3,20 @@
 /**
  * print_most_numbers - print num except 2 and 4
  *
- * Return: void
+ * Return: returns nothing
  */
 
 void print_most_numbers(void)
 {
-	char c;
+	int n;
 
-	for (c = '0'; c <= '9'; c++)
+	for (n = 48; n < 58; n++)
 	{
-		if (!(c == '0' || c == '4'))
-			_putchar(c);
+		if ((n == 50) || (n == 52))
+		{
+			continue;
+		}
+		putchar(n);
 	}
-	_putchar('\n');
+	putchar(10);
 }
