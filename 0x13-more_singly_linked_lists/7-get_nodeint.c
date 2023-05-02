47fd8af734ba3d17e9c8df81936d8c0e7d84c212
Adeluxx <getahunaderajew@gmail.com>
2023-05-02 04:21:26 -0700

    get_nodeint

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,8 +1,7 @@
 #include "lists.h"
 
 /**
-* get_nodeint_at_index - return nodes at a certain index in a linked
-list.
+* get_nodeint_at_index - return nodes at a certain index in a linked lists.
 * @head: first node in the linked list
 * @index: index of the node to return
 *
