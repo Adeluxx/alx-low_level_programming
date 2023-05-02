d9e5ec67da867caba46eb04c888a18e5e5df397a
Adeluxx <getahunaderajew@gmail.com>
2023-05-02 05:17:59 -0700

    free_listint_safe

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,8 +9,8 @@
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- * for alx project
+ * Description: singly linked list node structure.
+ *
  */
 typedef struct listint_s
 {
@@ -31,5 +31,7 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
